a7a4e3040dfc35160bb17b1650363d26775d63cc
Henry Oberholtzer <henryoberholtzer@gmail.com>
2024-03-11 09:03:29 -0700

    start symmetric difference

---
+++ symmetric-difference.js
@@ -0,0 +1,4 @@
+// From freeCodeCamp
+// Create a function that takes two or more arrays and returns an array of their symmetric difference.
+// The returned array must contain only unique values (No duplicates)
+// sym([1,2,3],[5,2,1,4]) should return [3,4,5]
